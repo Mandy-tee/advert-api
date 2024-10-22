c3acba2eb2d38c93b748e147efc3cdec102f9566
Amanda Tagoe <nayork77@gmail.com>
2024-10-22 16:20:40 +0000

    new advert api

---
+++ models/advertmodel.js
@@ -0,0 +1,17 @@
+// import { Schema, model } from "mongoose";
+
+// const todoSchema = new Schema({
+//     title: {type: String, required: true},
+//     icon: {type: String, required: true},
+//     completed: {type: Boolean, default: false}
+// });
+
+// todoSchema.plugin(toJSON);
+
+// export const TodoModel = model("Todo", todoSchema);
+
+import { Schema, model } from "mongoose";
+
+const advertSchema = new Schema({
+
+})
